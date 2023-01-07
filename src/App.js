a0a5b9f189f537bcb1bf2ff8f89051253e964627
Emmanuel-Ingwe <ingwemmanuel@gmail.com>
2023-01-07 23:50:05 +0100

    Footer

--- src/App.js
+++ src/App.js
@@ -31,7 +31,17 @@ const App = () => {
                 </Layout>
             </div>
             <div className="footer">
-
+                <Typography.Title level={5} style={{ color: 'white', textAlign: 'center' }}>Copyright © 2021
+                    <Link to="/">
+                        Cryptoverse Inc.
+                    </Link> <br />
+                    All Rights Reserved.
+                </Typography.Title>
+                <Space>
+                    <Link to="/">Home</Link>
+                    <Link to="/exchanges">Exchanges</Link>
+                    <Link to="/news">News</Link>
+                </Space>
             </div>
         </div>
     );
