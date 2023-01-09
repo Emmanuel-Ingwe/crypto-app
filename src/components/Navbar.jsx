a8d8535cd2cebe4daf593b6da6131524c508003a
Emmanuel-Ingwe <ingwemmanuel@gmail.com>
2023-01-09 14:28:17 +0100

    menu

--- src/components/Navbar.jsx
+++ src/components/Navbar.jsx
@@ -1,49 +1,25 @@
-import React, { useState, useEffect } from "react";
-import { Button, Menu, Typography, Avatar } from "antd";
+import React from "react";
+import { Menu, Typography, Avatar } from "antd";
 import { Link } from "react-router-dom";
 import {
 	HomeOutlined,
 	MoneyCollectOutlined,
 	BulbOutlined,
 	FundOutlined,
-	MenuOutlined,
 } from "@ant-design/icons";
 
 import icon from "../images/cryptocurrency.png";
 
-function getItem(label, key, icon, children, theme) {
-	return {
-		key,
-		icon,
-		children,
-		label,
-		theme,
-	};
-}
-
 const Navbar = () => {
-	// const [activeMenu, setActiveMenu] = useState(true);
-	// const [screenSize, setScreenSize] = useState(undefined);
-	const items = [
-		getItem("Navigation One", "sub1", <HomeOutlined />, [
-			getItem("Option 1", "1"),
-			getItem("Option 2", "2"),
-			getItem("Option 3", "3"),
-		]),
-		getItem("Option 5", "5"),
-		getItem("Option 6", "6"),
-	];
-
 	return (
 		<div className='nav-container'>
 			<div className='logo-container'>
-				<Avatar src={icon} size='large' />
+				<Avatar src={icon} />
 				<Typography.Title level={2} className='logo'>
 					<Link to='/'>Cryptoverse</Link>
 				</Typography.Title>
 			</div>
-			{/* {activeMenu && ( */}
-			{/* <Menu theme='dark'>
+			<Menu theme='dark'>
 				<Menu.Item icon={<HomeOutlined />}>
 					<Link to='/'>Home</Link>
 				</Menu.Item>
@@ -51,13 +27,12 @@ const Navbar = () => {
 					<Link to='/cryptocurrencies'>Cryptocurrencies</Link>
 				</Menu.Item>
 				<Menu.Item icon={<MoneyCollectOutlined />}>
-				<Link to='/exchanges'>Exchanges</Link>
+					<Link to='/exchanges'>Exchanges</Link>
 				</Menu.Item>
 				<Menu.Item icon={<BulbOutlined />}>
-				<Link to='/news'>News</Link>
+					<Link to='/news'>News</Link>
 				</Menu.Item>
-				</Menu>
-			)} */}
+			</Menu>
 		</div>
 	);
 };
